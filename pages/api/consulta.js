099f43163e931f0e28de70af2389e1dc343eb1c9
Andrews <adrewsnobre@gmail.com>
2021-04-06 12:22:44 -0300

    blockchain neu api

--- pages/api/consulta.js
+++ pages/api/consulta.js
@@ -1,170 +1,170 @@
 const Web3 = require("web3")
 
-async function consulta(request,response){ 
-   
-    const { hash } = request.query;
+async function consulta(request, response) {
 
-       var myContract = require('web3-eth-contract');  
+	const { hash } = request.query;
 
-  const provider = new Web3.providers.HttpProvider("https://rinkeby-light.eth.linkpool.io");
-//"https://rinkeby.infura.io/v3/1e2d6c8480ba48b69c9eace5b5b25211"
-const web3 = new Web3(provider);
-			
-web3.eth.defaultAccount = await web3.eth.accounts[0];
+	var myContract = require('web3-eth-contract');
 
-myContract = new web3.eth.Contract([
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"name": "birthdate",
-				"type": "string"
-			},
-			{
-				"name": "department",
-				"type": "string"
-			},
-			{
-				"name": "CPF",
-				"type": "string"
-			},
-			{
-				"name": "email",
-				"type": "string"
-			},
-			{
-				"name": "id",
-				"type": "string"
-			}
-		],
-		"name": "register",
-		"outputs": [
-			{
-				"name": "R",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "HashIds",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "novo",
-				"type": "string"
-			}
-		],
-		"name": "receber",
-		"outputs": [
-			{
-				"name": "a",
-				"type": "string"
-			},
-			{
-				"name": "b",
-				"type": "string"
-			},
-			{
-				"name": "c",
-				"type": "string"
-			},
-			{
-				"name": "d",
-				"type": "string"
-			},
-			{
-				"name": "e",
-				"type": "string"
-			},
-			{
-				"name": "f",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "novo",
-				"type": "string"
-			}
-		],
-		"name": "receber2",
-		"outputs": [
-			{
-				"name": "a",
-				"type": "string"
-			},
-			{
-				"name": "b",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-],'0xA0638824B89524ebF9c015cFeBc1442eB1111095');
+	const provider = new Web3.providers.HttpProvider("https://rinkeby-light.eth.linkpool.io");
+	//"https://rinkeby.infura.io/v3/1e2d6c8480ba48b69c9eace5b5b25211"
+	const web3 = new Web3(provider);
 
-			 	 
-		  var output1 = await myContract.methods.receber(hash).call((error, result) => {
-        
-		 if (result[0] == '') {
-			
-			 
-             response.json({
+	web3.eth.defaultAccount = await web3.eth.accounts[0];
 
-                Resposta: 'Arquivo inválido ou não registrado'
-            })
-			
-					
-	
-		}else
-  	
-	   response.json({
+	myContract = new web3.eth.Contract([
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "name",
+					"type": "string"
+				},
+				{
+					"name": "birthdate",
+					"type": "string"
+				},
+				{
+					"name": "department",
+					"type": "string"
+				},
+				{
+					"name": "CPF",
+					"type": "string"
+				},
+				{
+					"name": "email",
+					"type": "string"
+				},
+				{
+					"name": "id",
+					"type": "string"
+				}
+			],
+			"name": "register",
+			"outputs": [
+				{
+					"name": "R",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"name": "HashIds",
+			"outputs": [
+				{
+					"name": "",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "novo",
+					"type": "string"
+				}
+			],
+			"name": "receber",
+			"outputs": [
+				{
+					"name": "a",
+					"type": "string"
+				},
+				{
+					"name": "b",
+					"type": "string"
+				},
+				{
+					"name": "c",
+					"type": "string"
+				},
+				{
+					"name": "d",
+					"type": "string"
+				},
+				{
+					"name": "e",
+					"type": "string"
+				},
+				{
+					"name": "f",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "novo",
+					"type": "string"
+				}
+			],
+			"name": "receber2",
+			"outputs": [
+				{
+					"name": "a",
+					"type": "string"
+				},
+				{
+					"name": "b",
+					"type": "string"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		}
+	], '0xA0638824B89524ebF9c015cFeBc1442eB1111095');
 
-            metadado1: result[0],
-            metadado2: result[1],
-            metadado3: result[2],
-            metadado4: result[3],
-            metadado5: result[4],
-            metadado6: result[5],
-        })
-		
-		//alert(result[0]+ ','+result[1]); 
-});
 
-		
-		} 
+	var output1 = await myContract.methods.receber(hash).call((error, result) => {
+
+		if (result[0] == '') {
+
+
+			response.json({
+
+				Resposta: 'Arquivo inválido ou não registrado'
+			})
+
+
+
+		} else
+
+			response.json({
+
+				metadado1: result[0],
+				metadado2: result[1],
+				metadado3: result[2],
+				metadado4: result[3],
+				metadado5: result[4],
+				metadado6: result[5],
+			})
+
+		//alert(result[0]+ ','+result[1]);    ok
+	});
+
+
+}
 
 
 
@@ -176,7 +176,7 @@ const dynamicDate= new Date();
 
 response.json({
 
-    date: dynamicDate.toGMTString()
+	date: dynamicDate.toGMTString()
 })
 
 }*/
--- pages/api/gravar.js
+++ pages/api/gravar.js
@@ -1,203 +1,203 @@
 const Web3 = require("web3")
 
- function gravar(request,response){ 
-	const apiSecret =process.env.PKMINHA;
+function gravar(request, response) {
+	const apiSecret = process.env.PKMINHA;
 	if (request.method === 'POST') {
-				
+
 		var dados = request.body;
-		//res.status(200).json({ name: JJ.pid })
-	
-   // const { hash } = request.query;
+		//res.status(200).json({ name: JJ.pid })q
 
-       var myContract = require('web3-eth-contract');  
+		// const { hash } = request.query;
 
-  const provider = new Web3.providers.HttpProvider("https://rinkeby-light.eth.linkpool.io");
-//"https://rinkeby.infura.io/v3/1e2d6c8480ba48b69c9eace5b5b25211"
-const web3 = new Web3(provider);
-			
-web3.eth.defaultAccount =  web3.eth.accounts[0];
+		var myContract = require('web3-eth-contract');
 
-myContract = new web3.eth.Contract([
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "name",
-				"type": "string"
-			},
-			{
-				"name": "birthdate",
-				"type": "string"
-			},
-			{
-				"name": "department",
-				"type": "string"
-			},
-			{
-				"name": "CPF",
-				"type": "string"
-			},
-			{
-				"name": "email",
-				"type": "string"
-			},
-			{
-				"name": "id",
-				"type": "string"
-			}
-		],
-		"name": "register",
-		"outputs": [
-			{
-				"name": "R",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "HashIds",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "novo",
-				"type": "string"
-			}
-		],
-		"name": "receber",
-		"outputs": [
-			{
-				"name": "a",
-				"type": "string"
-			},
-			{
-				"name": "b",
-				"type": "string"
-			},
-			{
-				"name": "c",
-				"type": "string"
-			},
-			{
-				"name": "d",
-				"type": "string"
-			},
-			{
-				"name": "e",
-				"type": "string"
-			},
-			{
-				"name": "f",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "novo",
-				"type": "string"
-			}
-		],
-		"name": "receber2",
-		"outputs": [
-			{
-				"name": "a",
-				"type": "string"
-			},
-			{
-				"name": "b",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-],'0xA0638824B89524ebF9c015cFeBc1442eB1111095');
+		const provider = new Web3.providers.HttpProvider("https://rinkeby-light.eth.linkpool.io");
+		//"https://rinkeby.infura.io/v3/1e2d6c8480ba48b69c9eace5b5b25211"
+		const web3 = new Web3(provider);
 
-				  
+		web3.eth.defaultAccount = web3.eth.accounts[0];
+
+		myContract = new web3.eth.Contract([
+			{
+				"constant": false,
+				"inputs": [
+					{
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"name": "birthdate",
+						"type": "string"
+					},
+					{
+						"name": "department",
+						"type": "string"
+					},
+					{
+						"name": "CPF",
+						"type": "string"
+					},
+					{
+						"name": "email",
+						"type": "string"
+					},
+					{
+						"name": "id",
+						"type": "string"
+					}
+				],
+				"name": "register",
+				"outputs": [
+					{
+						"name": "R",
+						"type": "string"
+					}
+				],
+				"payable": false,
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [
+					{
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"name": "HashIds",
+				"outputs": [
+					{
+						"name": "",
+						"type": "string"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [
+					{
+						"name": "novo",
+						"type": "string"
+					}
+				],
+				"name": "receber",
+				"outputs": [
+					{
+						"name": "a",
+						"type": "string"
+					},
+					{
+						"name": "b",
+						"type": "string"
+					},
+					{
+						"name": "c",
+						"type": "string"
+					},
+					{
+						"name": "d",
+						"type": "string"
+					},
+					{
+						"name": "e",
+						"type": "string"
+					},
+					{
+						"name": "f",
+						"type": "string"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"constant": true,
+				"inputs": [
+					{
+						"name": "novo",
+						"type": "string"
+					}
+				],
+				"name": "receber2",
+				"outputs": [
+					{
+						"name": "a",
+						"type": "string"
+					},
+					{
+						"name": "b",
+						"type": "string"
+					}
+				],
+				"payable": false,
+				"stateMutability": "view",
+				"type": "function"
+			}
+		], '0xA0638824B89524ebF9c015cFeBc1442eB1111095');
+
+
 
 		const tx = {
-			from: '0x73A95592ddac218D0cA98eaf90bF168c3D45e883', to: '0xA0638824B89524ebF9c015cFeBc1442eB1111095', 
-			gas: 9000000, 
-		  //value: 1, 
-			data: myContract.methods.register(dados.metadado1,dados.metadado2,dados.metadado3,dados.metadado4,dados.metadado5,dados.hash).encodeABI() 
-			};
-				  
-				var SENHA= apiSecret;
-		const signPromise =  web3.eth.accounts.signTransaction(tx,SENHA);
-	// $(".resultado").html('Cadastro sendo efetuado na blockchain, por favor aguarde...');
-				
-		signPromise.then((signedTx) => {  
-	
-	  // raw transaction string may be available in .raw or 
-	  // .rawTransaction depending on which signTransaction
-	  // function was called
-	  const sentTx = web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);  
-	  
-	  sentTx.on("receipt", receipt => {
-		   
-		response.status(200).json({ name: 'Cadastrado com sucesso' });
-		// do something when receipt comes back
-		//	 $(".resultado").html('Arquivo registrado com sucesso!<br> Hash da transação: <a href=https://rinkeby.etherscan.io/tx/'+receipt.transactionHash+'>'+receipt.transactionHash+' </a><br><br>');
-			 
-	//	console.log(receipt);
-	//	console.log(receipt.transactionHash);
-			
-	  });
-	  
-	  sentTx.on("error", err => {
-		// do something on transaction error
-		console.log(err);
-	  });
-	  
-	}).catch((err) => {
-	  
-	  // do something when promise fails
-	  console.log("erro2");
-	  
-	});	
+			from: '0x73A95592ddac218D0cA98eaf90bF168c3D45e883', to: '0xA0638824B89524ebF9c015cFeBc1442eB1111095',
+			gas: 9000000,
+			//value: 1, 
+			data: myContract.methods.register(dados.metadado1, dados.metadado2, dados.metadado3, dados.metadado4, dados.metadado5, dados.hash).encodeABI()
+		};
+
+		var SENHA = apiSecret;
+		const signPromise = web3.eth.accounts.signTransaction(tx, SENHA);
+		// $(".resultado").html('Cadastro sendo efetuado na blockchain, por favor aguarde...');
+
+		signPromise.then((signedTx) => {
+
+			// raw transaction string may be available in .raw or 
+			// .rawTransaction depending on which signTransaction
+			// function was called
+			const sentTx = web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
+
+			sentTx.on("receipt", receipt => {
+
+				response.status(200).json({ name: 'Cadastrado com sucesso' });
+				// do something when receipt comes back
+				//	 $(".resultado").html('Arquivo registrado com sucesso!<br> Hash da transação: <a href=https://rinkeby.etherscan.io/tx/'+receipt.transactionHash+'>'+receipt.transactionHash+' </a><br><br>');
+
+				//	console.log(receipt);
+				//	console.log(receipt.transactionHash);
+
+			});
+
+			sentTx.on("error", err => {
+				// do something on transaction error
+				console.log(err);
+			});
+
+		}).catch((err) => {
+
+			// do something when promise fails
+			console.log("erro2");
+
+		});
 
 
 
 
 
 
-		
+
 		//alert(result[0]+ ','+result[1]); 
-};
+	};
 
-		
+
 
 
 }
 
-	
+
 /*
 function tempo(request,response){
 
@@ -205,7 +205,7 @@ const dynamicDate= new Date();
 
 response.json({
 
-    date: dynamicDate.toGMTString()
+	date: dynamicDate.toGMTString()
 })
 
 }*/
