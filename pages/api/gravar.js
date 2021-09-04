423d0917982d7bf79a630974d85d7a89aaf06d09
Everblock <andrews.rodrigues@everblock.com.br>
2021-09-04 19:28:08 -0300

    first commi 2t

--- pages/api/gravar.js
+++ pages/api/gravar.js
@@ -1,7 +1,8 @@
- const Web3 = require("web3")
+const Web3 = require("web3")
 
- function gravar(request, response) {
+ async function gravar(request, response) {
 	const apiSecret = process.env.PKMINHA;
+	var X;
 	if (request.method === 'POST') {
 
 		var dados = request.body;
@@ -11,11 +12,12 @@
 
 		var myContract = require('web3-eth-contract');
 
-		const provider = new Web3.providers.HttpProvider("https://rinkeby-light.eth.linkpool.io");
+		const provider = new Web3.providers.HttpProvider("https://rpc.gochain.io");
+		//https://rinkeby-light.eth.linkpool.io
 		//"https://rinkeby.infura.io/v3/1e2d6c8480ba48b69c9eace5b5b25211"
 		const web3 = new Web3(provider);
 
-		web3.eth.defaultAccount = web3.eth.accounts[0];
+		web3.eth.defaultAccount = await web3.eth.accounts[0];
 
 		myContract = new web3.eth.Contract([
 			{
@@ -138,19 +140,20 @@
 				"stateMutability": "view",
 				"type": "function"
 			}
-		], '0xA0638824B89524ebF9c015cFeBc1442eB1111095');
+		], '0xcBcf0f9b62BBaEcf1a74Cd0dc031a7dA48bb38D5');
+		//0xA0638824B89524ebF9c015cFeBc1442eB1111095npn start
 
 
 
-		const tx = {
-			from: '0x73A95592ddac218D0cA98eaf90bF168c3D45e883', to: '0xA0638824B89524ebF9c015cFeBc1442eB1111095',
+		 const  tx = {
+			from: '0x73A95592ddac218D0cA98eaf90bF168c3D45e883', to: '0xcBcf0f9b62BBaEcf1a74Cd0dc031a7dA48bb38D5',
 			gas: 9000000,
 			//value: 1, 
 			data: myContract.methods.register(dados.metadado1, dados.metadado2, dados.metadado3, dados.metadado4, dados.metadado5, dados.hash).encodeABI()
 		};
 
 		var SENHA = apiSecret;
-		const signPromise = web3.eth.accounts.signTransaction(tx, SENHA);
+		 const signPromise =  web3.eth.accounts.signTransaction(tx, SENHA);
 		// $(".resultado").html('Cadastro sendo efetuado na blockchain, por favor aguarde...');
 
 		signPromise.then((signedTx) => {
@@ -158,15 +161,15 @@
 			// raw transaction string may be available in .raw or 
 			// .rawTransaction depending on which signTransaction
 			// function was called
-			const sentTx = web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
+			const sentTx =  web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
 
-			sentTx.on("receipt", receipt => {
+			  sentTx.on("receipt", receipt => {
+				
+				 response.status(200).json({
 
-				response.status(200).json({ 
-					
 					Status: 'Cadastrado com sucesso',
 					Txhash: receipt.transactionHash
-			 });
+				});
 				// do something when receipt comes back
 				//	 $(".resultado").html('Arquivo registrado com sucesso!<br> Hash da transação: <a href=https://rinkeby.etherscan.io/tx/'+receipt.transactionHash+'>'+receipt.transactionHash+' </a><br><br>');
 
@@ -176,7 +179,10 @@
 			});
 
 			sentTx.on("error", err => {
-				// do something on transaction error
+				response.status(404).json({
+
+					Status: 'erro'
+				});
 
 				console.log(err);
 			});
@@ -184,19 +190,21 @@
 		}).catch((err) => {
 
 			// do something when promise fails
+			response.status(200).json({
+
+				Status: 'erro'
+			});
 			console.log("errodd2");
 
 		});
 
 
-
-
-
-
+		//return response.status(405).end();
+		
+		//return await response.status(405).end();
 
 		//alert(result[0]+ ','+result[1]); 
 	};
-
 
 
 
